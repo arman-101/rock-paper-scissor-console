d686b1044e17b109439ae6f52580d0a9604b8de0
Arman <78878938+arman-101@users.noreply.github.com>
2025-06-26 20:04:01 +0100

    added 5 rounds and grouped logic into functions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
 // computer choice
 
 function compChoice() {
-
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
@@ -14,47 +16,76 @@ function compChoice() {
   return choice;
 }
 
-const computerChoice = compChoice();
-
 // human choice
 
-const humanChoice = prompt("Choose between 'rock' 'paper' 'scissors': ").toLowerCase();
-console.log(humanChoice);
+function getHumanChoice() {
+  const humanChoice = prompt(
+    "Choose between 'rock' 'paper' 'scissors': "
+  ).toLowerCase();
+  console.log(humanChoice);
+  return humanChoice;
+}
 
 // logic and output
 
-if (humanChoice === computerChoice) {
-  console.log("Draw!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
+function playGame(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Draw!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+
+    console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("Human Wins!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    humanScore++;
+        console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("Human Wins!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    humanScore++;
+        console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("Human Wins!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    humanScore++;
+        console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  } else if (computerChoice === "rock" && humanChoice === "scissors") {
+    console.log("Computer Wins!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    computerScore++;
+        console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  } else if (computerChoice === "paper" && humanChoice === "rock") {
+    console.log("Computer Wins!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    computerScore++;
+        console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  } else if (computerChoice === "scirros" && humanChoice === "paper") {
+    console.log("Computer Wins!");
+    console.log(`Human chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+    computerScore++;
+        console.log(`Human score is ${humanScore}`)
+    console.log(`Computer score is ${computerScore}`)
+  }
 }
-else if (humanChoice === "rock" && computerChoice === "scissors") {
-  console.log("Human Wins!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-}
-else if (humanChoice === "paper" && computerChoice === "rock") {
-  console.log("Human Wins!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-}
-else if (humanChoice === "scissors" && computerChoice === "paper") {
-  console.log("Human Wins!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-}
-else if (computerChoice === "rock" && humanChoice === "scissors") {
-  console.log("Computer Wins!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-}
-else if (computerChoice === "paper" && humanChoice === "rock") {
-  console.log("Computer Wins!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-}
-else if (computerChoice === "scirros" && humanChoice === "paper") {
-  console.log("Computer Wins!");
-  console.log(`Human chose: ${humanChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
+
+// play 5 times
+
+for (let i = 0; i < 5; i++) {
+  const humanChoice = getHumanChoice();
+  const computerChoice = compChoice();
+
+  playGame(humanChoice, computerChoice);
 }
