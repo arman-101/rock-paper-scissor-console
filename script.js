d1bd565ee59783a024dfeda7cb1f4468d7818859
Arman <78878938+arman-101@users.noreply.github.com>
2025-06-26 19:48:39 +0100

    added if's for logic

--- script.js
+++ script.js
@@ -11,11 +11,50 @@ function compChoice() {
   let choice = choices[numbChoice];
 
   console.log(choice);
+  return choice;
 }
 
-compChoice();
+const computerChoice = compChoice();
 
 // human choice
 
+const humanChoice = prompt("Choose between 'rock' 'paper' 'scissors': ").toLowerCase();
+console.log(humanChoice);
 
 // logic and output
+
+if (humanChoice === computerChoice) {
+  console.log("Draw!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
+else if (humanChoice === "rock" && computerChoice === "scissors") {
+  console.log("Human Wins!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
+else if (humanChoice === "paper" && computerChoice === "rock") {
+  console.log("Human Wins!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
+else if (humanChoice === "scissors" && computerChoice === "paper") {
+  console.log("Human Wins!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
+else if (computerChoice === "rock" && humanChoice === "scissors") {
+  console.log("Computer Wins!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
+else if (computerChoice === "paper" && humanChoice === "rock") {
+  console.log("Computer Wins!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
+else if (computerChoice === "scirros" && humanChoice === "paper") {
+  console.log("Computer Wins!");
+  console.log(`Human chose: ${humanChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+}
